5e4f56554922776bee90a0d9f85cb7381aea835e
Jeremiah81505 <elevatedsurfacesgb@gmail.com>
2025-08-23 02:35:48 -0500

    Register hero patterns with static metadata

---
+++ inc/patterns/hero-intro.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Pattern: Hero Intro
+ */
+
+if ( ! function_exists( 'register_block_pattern' ) ) {
+    return;
+}
+
+ob_start();
+include get_theme_file_path( 'patterns/hero-intro.php' );
+$pattern_content = ob_get_clean();
+
+register_block_pattern(
+    'kadence-child/hero-intro',
+    [
+        'title'       => __( 'Hero Intro', 'kadence-child' ),
+        'description' => __( 'Simple hero intro section with heading and paragraph.', 'kadence-child' ),
+        'categories'  => [ 'kadence-child', 'text', 'featured' ],
+        'content'     => $pattern_content,
+    ]
+);
