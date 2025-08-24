79c8e80918711884bd2ff92e85c0c53ba641da76
Jeremiah81505 <elevatedsurfacesgb@gmail.com>
2025-08-24 15:08:34 -0500

    Add translation-ready materials grid template

--- inc/patterns/es-mats-grid.php
+++ inc/patterns/es-mats-grid.php
@@ -14,71 +14,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 $include_path = get_theme_file_path( 'patterns/es-mats-grid.php' );
 
 if ( file_exists( $include_path ) ) {
-	ob_start();
-	include $include_path;
-	$pattern_content = ob_get_clean();
+        ob_start();
+        include $include_path;
+        $pattern_content = ob_get_clean();
 } else {
-	$pattern_content = <<<HTML
-<!-- wp:group {"tagName":"section","layout":{"type":"constrained"}} -->
-<section class="wp-block-group">
-<!-- wp:html -->
-<div id="es-mats" data-replay="true" data-stagger="140">
-  <div class="es-grid">
-
-    <!-- QUARTZ (hero: col 1, rows 1–2) -->
-    <a href="https://elevatedcountertopexperts.com/quartz/" class="es-card cell-quartz" data-dir="left">
-      <img src="https://elevatedcountertopexperts.com/wp-content/uploads/2025/08/VIATERA-Residential-Taj-Crema-kitchen-Mid2-scaled.jpg" alt="Quartz countertop">
-      <span class="tint"></span>
-      <span class="card-title">Quartz</span>
-      <span class="card-index">01</span>
-    </a>
-
-    <!-- NATURAL STONE -->
-    <a href="https://elevatedcountertopexperts.com/natural-stone/" class="es-card cell-ns" data-dir="up">
-      <img src="https://elevatedcountertopexperts.com/wp-content/uploads/2025/08/Vertical-Application-of-Natural-Stone-Kitchen-Lakewood-CO-1-1.jpeg" alt="Natural stone countertop">
-      <span class="tint"></span>
-      <span class="card-title">Natural&nbsp;Stone</span>
-      <span class="card-index">02</span>
-    </a>
-
-    <!-- SOLID SURFACE -->
-    <a href="https://elevatedcountertopexperts.com/solid-surface/" class="es-card cell-solid" data-dir="down">
-      <img src="https://elevatedcountertopexperts.com/wp-content/uploads/2025/08/Solid-Surface-1.jpg" alt="Solid surface countertop">
-      <span class="tint"></span>
-      <span class="card-title">Solid&nbsp;Surface</span>
-      <span class="card-index">03</span>
-    </a>
-
-    <!-- ULTRA COMPACT (wide middle) -->
-    <a href="https://elevatedcountertopexperts.com/ultra-compact/" class="es-card cell-ultra" data-dir="right">
-      <img src="https://elevatedcountertopexperts.com/wp-content/uploads/2025/08/Dekton-Countertops.avif" alt="Ultra compact surface">
-      <span class="tint"></span>
-      <span class="card-title">Ultra&nbsp;Compact</span>
-      <span class="card-index">04</span>
-    </a>
-
-    <!-- LAMINATE (bottom-left wide) -->
-    <a href="https://elevatedcountertopexperts.com/laminate/" class="es-card cell-laminate" data-dir="up">
-      <img src="https://elevatedcountertopexperts.com/wp-content/uploads/2025/08/Formica-7404-Neapolitan-Stone-3-scaled_4aaa1f1a-c749-4986-97ce-57f9ddcdcf1b_1080x.webp" alt="Laminate countertop">
-      <span class="tint"></span>
-      <span class="card-title">Laminate</span>
-      <span class="card-index">05</span>
-    </a>
-
-    <!-- SINKS (bottom-right) -->
-    <a href="https://elevatedcountertopexperts.com/sinks/" class="es-card cell-sinks" data-dir="left">
-      <img src="https://elevatedcountertopexperts.com/wp-content/uploads/2025/08/Undermount-SInk-Karran.jpg" alt="Kitchen sinks">
-      <span class="tint"></span>
-      <span class="card-title">Sinks</span>
-      <span class="card-index">06</span>
-    </a>
-
-  </div>
-</div>
-<!-- /wp:html -->
-</section>
-<!-- /wp:group -->
-HTML;
+        ob_start();
+        include get_theme_file_path( 'inc/patterns/es-mats-grid-template.php' );
+        $pattern_content = ob_get_clean();
 }
 
 if ( function_exists( 'register_block_pattern' ) ) {
